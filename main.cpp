4c3b8f4b5458498a21d1cdd95545fac3a4a07393
Jada Smith <jada.smith@lethpolytech.ca>
2024-11-04 08:24:10 -0700

    cool shit bruh

--- main.cpp
+++ main.cpp
@@ -21,11 +21,17 @@ int main() {
 	// if structure is in main, its local to main
 	
 	FloorHockeyTeam team1;
-	team1.name = "Kodiaks";
+	team1.name = "Kodiaks"; // thingOnTheRight is a member of the ThingOnTheLeft
 	team1.wins = 10;
 	team1.losses = 0;
 	team1.ties = 0;
 	team1.totalGoalsFor = 37;
+
+
+	FloorHockeyTeam team2 = {"Dinos", 9, 0, 1, 31}; // can list them like araay. order matters
+
+	cout << team1.name << " goals for: " << team1.totalGoalsFor << endl;
+	cout << team2.name << " goals for: " << team2.totalGoalsFor << endl;
 
 
 	return 0;
