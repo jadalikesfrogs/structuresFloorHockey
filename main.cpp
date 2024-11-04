5a9350c4afc50e33d3806065eeda53d265e6cc43
Jada Smith <jada.smith@lethpolytech.ca>
2024-11-04 08:20:37 -0700

    these are cool

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 /*
 Intro to structures
 */
-
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
+struct FloorHockeyTeam { // structs/classes use capital letter follow the style guide
+	string name;
+	int wins;
+	int losses;
+	int ties;
+	int totalGoalsFor;
+	vector<string> players;
+	vector<string> positions;
+
+}; // this is a statement so needs a semicolon
+
 int main() {
+	// if structure is in main, its local to main
+	
+	FloorHockeyTeam team1;
+	team1.name = "Kodiaks";
+	team1.wins = 10;
+	team1.losses = 0;
+	team1.ties = 0;
+	team1.totalGoalsFor = 37;
 
 
+	return 0;
 }
